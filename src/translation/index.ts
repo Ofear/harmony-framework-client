dc53259b0ff1e4e7a863177c5d849904ea9a49a9
refaelok <refaeloknin@gmail.com>
2020-05-16 14:29:12 +0300

    fix templates

--- src/translation/index.ts
+++ src/translation/index.ts
@@ -6,7 +6,6 @@ export const languageTypes: string[] | NamedLanguage[] = ['en', 'fr'];
 
 export const translations = {
 	deviceGallery: {
-		x: 'y',
 		addToCartButton: [en.deviceGallery.addToCartButton, fr.deviceGallery.addToCartButton],
 		priceTitle: [en.deviceGallery.priceTitle, fr.deviceGallery.priceTitle]
 	}
