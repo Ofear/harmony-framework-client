ec9c16afdd9a0cca72e32a02689ef37727b8ad25
refaelok <refaeloknin@gmail.com>
2020-05-18 15:23:50 +0300

    fix navigate to home page

--- src/base/features/base-global-spinner/index.tsx
+++ src/base/features/base-global-spinner/index.tsx
@@ -11,14 +11,14 @@ import * as spinnerConfig from 'configurations/spinner.config.json';
 			const matchItem = _.filter(spinnerConfig.ignoreList, (regx) => url.match(regx));
 
 			if (this.readyState === this.OPENED) {
-				if (matchItem && matchItem.length) {
+				if (!matchItem || !matchItem.length) {
 					store.dispatch({
 						type: 'XHR_TASK_START',
 						[pendingTask]: begin
 					});
 				}
 			} else if (this.readyState === this.DONE) {
-				if (matchItem && matchItem.length) {
+				if (!matchItem || !matchItem.length) {
 					store.dispatch({
 						type: 'XHR_TASK_END',
 						[pendingTask]: end
